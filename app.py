e5723972e7a6389136621dc43a3a18c3ab9a8cc8
Gowtham2543 <gowtham.manoharan@outlook.com>
2023-06-18 23:10:59 +0530

    Testing update

--- app.py
+++ app.py
@@ -287,13 +287,13 @@ def employee_details_update():
         
     data = request.form
     print(data)
-    if not data or not data.get('email') or not data.get('firstName') or not data.get('lastName') or not data.get('dob') or not data.get('designation') or not data.get('age'):
+    if not data or not data.get('email') or not data.get('first_name') or not data.get('last_name') or not data.get('dob') or not data.get('designation') or not data.get('age'):
         return make_response(
             jsonify({"status" : "Fields missing"}),
             401)
         
-    first_name = data.get('firstName')
-    last_name = data.get('lastName')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
     dob = data.get('dob')
     age = data.get('age')
     email = data.get('email')
